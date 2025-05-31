2597f4ca0eb81c88bb61bd0564b02c2803751abf
Cynthion <cynthion21x@gmail.com>
2025-05-31 13:31:33 +0100

    new command line arguments system

--- src/Main.hs
+++ src/Main.hs
@@ -10,55 +10,91 @@ import I3IPC
 import qualified I3IPC.Subscribe as Sub
 import qualified I3IPC.Event as E
 import qualified I3IPC.Reply as R
+import qualified Data.Text as T
 import Control.Monad (void, forever, when)
 import Control.Concurrent (threadDelay)
 import Network.Socket (Socket)
+import System.Environment (getArgs)
 
 appVersion :: String
-appVersion = "0.1.0.0"
+appVersion = "0.2.0.0"
+
+data Config = Config
+    { verbose :: Bool
+    , ignoreWindows :: [String]
+    , threadTime :: Int
+    }
+
+defaultConfig :: Config
+defaultConfig = Config
+    { verbose = False
+    , ignoreWindows = ["xfce4-notifyd", "dunst"]
+    , threadTime = 1000000000
+    }
+
+parseArgs :: [String] -> Config
+parseArgs x = process x defaultConfig
+
+    where process [] config = config
+          process ("-v":xs) config = process xs config { verbose = True }
+          process ("-i":arg:xs) config = process xs config { ignoreWindows = splitWindows arg } 
+          process ("-t":arg:xs) config = process xs config { threadTime = read arg }
+          process (_:xs) config = process xs config
+
+          splitWindows :: String -> [String]
+          splitWindows = words . map (\c -> if c == ',' then ' ' else c)
 
 main :: IO ()
 main = do
 
-    conn <- connecti3
+    args <- getArgs
+    let config = parseArgs args
+
+    when (verbose config) $ do
+        putStrLn ("Arguments: " ++ (show args))
+        putStrLn $ "Ignoring windows: " ++ show (ignoreWindows config)
 
     putStrLn ("I3-Auto-Split v" ++ appVersion)
+
+    conn <- connecti3
     putStrLn ("Connected to i3 :D")
 
-    subscribe (handler conn) [Sub.Window]
+    subscribe (handler config conn) [Sub.Window]
 
-    forever $ threadDelay 1000000000
+    forever $ threadDelay (threadTime config) 
 
-handler :: Socket -> Either String E.Event -> IO ()
-handler conn (Right (E.Window ev)) 
-    | E.win_change ev == E.WinNew = handleNewWin conn ev
+handler :: Config -> Socket -> Either String E.Event -> IO ()
+handler config conn (Right (E.Window ev)) 
+    | E.win_change ev == E.WinNew = handleNewWin config conn ev
     | otherwise = return () 
-handler _ (Left err) = putStrLn $ "Error :c : " ++ err
-handler _ _ = return ()
+handler _ _ (Left err) = putStrLn $ "Error :c : " ++ err
+handler _ _ _ = return ()
 
-handleNewWin :: Socket -> E.WindowEvent -> IO ()
-handleNewWin conn ev = do
+handleNewWin :: Config -> Socket -> E.WindowEvent -> IO ()
+handleNewWin config conn ev = do
 
-    let node = E.win_container ev
-    let nodeType = R.node_type node
-    let nodeName = R.node_name node
+    when (verbose config) $ do
 
-    putStrLn $ "New window type: " ++ show nodeType
-    putStrLn $ "New window name: " ++ show nodeName
+        let node = E.win_container ev
+        let nodeType = R.node_type node
+        let nodeName = R.node_name node
+
+        putStrLn $ "New window type: " ++ show nodeType
+        putStrLn $ "New window name: " ++ show nodeName
     
-    when (isTilingWindow ev) $ do
+    when (isTilingWindow config ev) $ do
         putStrLn "Handling new window"
         void $ runCommand conn "split toggle"
 
-isTilingWindow :: E.WindowEvent -> Bool
-isTilingWindow ev = nodeType /= R.FloatingConType && 
-                    not (isNotification)
+isTilingWindow :: Config -> E.WindowEvent -> Bool
+isTilingWindow config ev = nodeType /= R.FloatingConType && 
+                           not (isNotification)
     where node = E.win_container ev
           nodeName = R.node_name node
           nodeType = R.node_type node
 
           isNotification = case nodeName of
               
-              Just name -> name `elem` ["xfce4-notifyd", "dunst"] 
+              Just name -> T.unpack name `elem` (ignoreWindows config)
               Nothing -> False
 
